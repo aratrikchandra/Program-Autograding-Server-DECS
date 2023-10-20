a61e4547a43c2cddab6074343990a7cc5eb12936
aratrik1999 <46664969+aratrikchandra@users.noreply.github.com>
2023-10-20 16:48:54 +0530

    Update gradingclient.c for loadtest

--- lab7/gradingclient.c
+++ lab7/gradingclient.c
@@ -4,28 +4,35 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <netdb.h>
-
-void error(char *msg) {
+#include <sys/time.h>
+void error(char *msg)
+{
     perror(msg);
     exit(1);
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
-    if (argc < 3) {
-        fprintf(stderr, "Usage: %s <serverIP:port> <sourceCodeFileTobeGraded>\n", argv[0]);
+    if (argc < 5)
+    {
+        fprintf(stderr, "Usage: %s <serverIP:port> <sourceCodeFileTobeGraded> <loopNum> <sleepTimeSeconds>\n", argv[0]);
         exit(1);
     }
+
+    int loopNum = atoi(argv[3]);
+    int sleepTimeSeconds = atoi(argv[4]);
 
     portno = atoi(strchr(argv[1], ':') + 1);
     char server_ip[256];
     strncpy(server_ip, argv[1], strchr(argv[1], ':') - argv[1]);
     server = gethostbyname(server_ip);
 
-    if (server == NULL) {
+    if (server == NULL)
+    {
         fprintf(stderr, "ERROR, no such host\n");
         exit(1);
     }
@@ -43,35 +50,74 @@ int main(int argc, char *argv[]) {
         error("ERROR connecting to server");
 
     // Read source code from file
-    FILE *source_file = fopen(argv[2], "r");
-    if (source_file == NULL)
-        error("ERROR opening source code file");
+ struct timeval Tsend, Trecv;
+    double sum_response_time = 0;
+    int successful_responses = 0;
 
-    char buffer[256];
-    size_t bytes_read;
-    while ((bytes_read = fread(buffer, 1, sizeof(buffer), source_file)) > 0) {
-        n = write(sockfd, buffer, bytes_read);
-        if (n < 0)
-            error("ERROR writing to socket");
+struct timeval Tstart, Tend;
+    gettimeofday(&Tstart, NULL); // Get the time before starting the loop
+    for (int i = 0; i < loopNum; i++)
+    {
+        // Read source code from file
+        FILE *source_file = fopen(argv[2], "r");
+        if (source_file == NULL)
+            error("ERROR opening source code file");
+
+        char buffer[256];
+        size_t bytes_read;
+        fseek(source_file, 0L, SEEK_END);
+        int length = ftell(source_file);
+        fseek(source_file, 0L, SEEK_SET);
+        write(sockfd, &length, sizeof(int));
+        while ((bytes_read = fread(buffer, 1, sizeof(buffer), source_file)) > 0)
+        {
+            int n = write(sockfd, buffer, bytes_read);
+            if (n < 0)
+                error("ERROR writing to socket");
+        }
+        fclose(source_file);
+
+        gettimeofday(&Tsend, NULL); // Get the time before sending the request
+
+        int flag=1;
+        while (1)
+        {
+            // Receive and print response from server
+            bzero(buffer, 256);
+            n = read(sockfd, buffer, 255);
+            if (n < 0)
+            {
+                error("ERROR reading from socket");
+                flag=0;
+            }
+                
+
+            // Break loop if end of transmission signal is received
+            if (strcmp(buffer, "END\n") == 0)
+                break;
+
+            printf("-->%s\n", buffer);
+        }
+
+        gettimeofday(&Trecv, NULL); // Get the time after receiving the response
+
+        double response_time = (Trecv.tv_sec - Tsend.tv_sec) + (Trecv.tv_usec - Tsend.tv_usec) / 1000000.0;
+        sum_response_time += response_time;
+        if(flag)
+        successful_responses++;
+
+        sleep(sleepTimeSeconds); // Sleep for the specified amount of time
     }
 
-    fclose(source_file);
+    gettimeofday(&Tend, NULL); // Get the time after finishing the loop
 
-while (1) {
-    // Receive and print response from server
-    bzero(buffer, 256);
-    n = read(sockfd, buffer, 255);
-    if (n < 0)
-        error("ERROR reading from socket");
-    
-    // Break loop if end of transmission signal is received
-    if (strcmp(buffer, "END\n") == 0)
-        break;
+    double total_time = (Tend.tv_sec - Tstart.tv_sec) + (Tend.tv_usec - Tstart.tv_usec) / 1000000.0;
+    printf("Total time taken to complete the loop: %.6f seconds\n", total_time);
 
-    printf("-->%s\n", buffer);
-}
-
+    printf("Average response time: %.6f seconds\n", sum_response_time / successful_responses);
+    printf("Number of successful responses: %d\n", successful_responses);
 
     close(sockfd);
     return 0;
 }
+
