4b5681966da1ad777f80d3d8d78da7fa79c9652c
aratrik1999 <46664969+aratrikchandra@users.noreply.github.com>
2023-10-21 23:57:09 +0530

    changing client structure

--- lab7/gradingclient.c
+++ lab7/gradingclient.c
@@ -37,27 +37,27 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-        error("ERROR opening socket");
-
     bzero((char *)&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     memcpy(&serv_addr.sin_addr, server->h_addr_list[0], server->h_length);
     serv_addr.sin_port = htons(portno);
 
-    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-        error("ERROR connecting to server");
-
     // Read source code from file
- struct timeval Tsend, Trecv;
+    struct timeval Tsend, Trecv;
     double sum_response_time = 0;
     int successful_responses = 0;
 
-struct timeval Tstart, Tend;
+    struct timeval Tstart, Tend;
     gettimeofday(&Tstart, NULL); // Get the time before starting the loop
     for (int i = 0; i < loopNum; i++)
     {
+        sockfd = socket(AF_INET, SOCK_STREAM, 0);
+        if (sockfd < 0)
+            error("ERROR opening socket");
+
+        if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+            error("ERROR connecting to server");
+
         // Read source code from file
         FILE *source_file = fopen(argv[2], "r");
         if (source_file == NULL)
@@ -79,34 +79,29 @@ struct timeval Tstart, Tend;
 
         gettimeofday(&Tsend, NULL); // Get the time before sending the request
 
-        int flag=1;
-        while (1)
+        int flag = 1;
+
+        // Receive and print response from server
+        bzero(buffer, 256);
+        n = read(sockfd, buffer, 255);
+        if (n < 0)
         {
-            // Receive and print response from server
-            bzero(buffer, 256);
-            n = read(sockfd, buffer, 255);
-            if (n < 0)
-            {
-                error("ERROR reading from socket");
-                flag=0;
-            }
-                
-
-            // Break loop if end of transmission signal is received
-            if (strcmp(buffer, "END\n") == 0)
-                break;
-
-            printf("-->%s\n", buffer);
+            error("ERROR reading from socket");
+            flag = 0;
         }
+
+        printf("-->%s\n", buffer);
 
         gettimeofday(&Trecv, NULL); // Get the time after receiving the response
 
         double response_time = (Trecv.tv_sec - Tsend.tv_sec) + (Trecv.tv_usec - Tsend.tv_usec) / 1000000.0;
         sum_response_time += response_time;
-        if(flag)
-        successful_responses++;
+        if (flag)
+            successful_responses++;
 
         sleep(sleepTimeSeconds); // Sleep for the specified amount of time
+
+        close(sockfd);
     }
 
     gettimeofday(&Tend, NULL); // Get the time after finishing the loop
@@ -120,4 +115,3 @@ struct timeval Tstart, Tend;
     close(sockfd);
     return 0;
 }
-
