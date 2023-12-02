5768b043378a5603fd9190b449f73fc2700501ae
bohemian-bong <147497706+bohemian-bong@users.noreply.github.com>
2023-12-02 13:19:18 +0530

    Minor Bug Fixes

--- lab7/plot.py
+++ lab7/plot.py
@@ -22,41 +22,5 @@ plt.ylabel('Response Time')
 plt.grid(True)
 plt.savefig('result/ResponseTime_vs_Clients.pdf')
 
-#Plot Goodput vs Clients
-plt.figure(figsize=(10, 5))
-plt.plot(data['Clients'], data['Goodput'], marker='o', color='g')
-plt.title('Goodput vs Clients')
-plt.xlabel('Clients')
-plt.ylabel('Goodput')
-plt.grid(True)
-plt.savefig('result/Goodput_vs_Clients.pdf')
-
-#Plot TimeoutRate vs Clients
-plt.figure(figsize=(10, 5))
-plt.plot(data['Clients'], data['TimeoutRate'], marker='o', color='b')
-plt.title('TimeoutRate vs Clients')
-plt.xlabel('Clients')
-plt.ylabel('TimeoutRate')
-plt.grid(True)
-plt.savefig('result/TimeoutRate_vs_Clients.pdf')
-
-#Plot ErrorRate vs Clients
-plt.figure(figsize=(10, 5))
-plt.plot(data['Clients'], data['ErrorRate'], marker='o', color='r')
-plt.title('ErrorRate vs Clients')
-plt.xlabel('Clients')
-plt.ylabel('ErrorRate')
-plt.grid(True)
-plt.savefig('result/ErrorRate_vs_Clients.pdf')
-
-# Plot RequestSentRate vs Clients
-plt.figure(figsize=(10, 5))
-plt.plot(data['Clients'], data['RequestSentRate'], marker='o', color='g')
-plt.title('RequestSentRate vs Clients')
-plt.xlabel('Clients')
-plt.ylabel('RequestSentRate')
-plt.grid(True)
-plt.savefig('result/RequestSentRate_vs_Clients.pdf')
-
-print("The plots have been saved as 'results' directory")
+print("The plots have been saved as 'Throughput_vs_Clients.pdf' and 'ResponseTime_vs_Clients.pdf'.")
 
